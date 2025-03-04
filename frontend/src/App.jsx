f54024dd9d655c33f4703d183ae491e5335e9bb1
cherubkia <kirubelmelaku2019@gmail.com>
2025-03-04 20:08:27 +0300

    Added Footer component and modified AuthScreen and App components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import Footer from "./components/Footer";
 
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/Login" element={<LoginPage />} />
-      <Route path="/Signup" element={<SignUpPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignUpPage />} />
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
